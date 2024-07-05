25f2621e1c11790cce64d9adef14a71dc5b0124e
Tejas More <tejasmore660@gmail.com>
2024-07-05 12:36:34 +0530

    flag animation

--- script2.js
+++ script2.js
@@ -2,20 +2,20 @@ function locomotiveScroll() {
     gsap.registerPlugin(ScrollTrigger);
 
     const locoScroll = new LocomotiveScroll({
-        el: document.querySelector("main"),
+        el: document.querySelector(".main"),
         smooth: true
     });
 
     locoScroll.on("scroll", ScrollTrigger.update);
 
-    ScrollTrigger.scrollerProxy("main", {
+    ScrollTrigger.scrollerProxy(".main", {
         scrollTop(value) {
             return arguments.length ? locoScroll.scrollTo(value, 0, 0) : locoScroll.scroll.instance.scroll.y;
         },
         getBoundingClientRect() {
             return {top: 0, left: 0, width: window.innerWidth, height: window.innerHeight};
         },
-        pinType: document.querySelector("main").style.transform ? "transform" : "fixed"
+        pinType: document.querySelector(".main").style.transform ? "transform" : "fixed"
     });
 
     ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
@@ -199,29 +199,33 @@ function sheryanimations() {
     });
 }
 
-document.addEventListener("mousemove",(det)=>{
-    gsap.to("#flag", {
-        x: det.clientX - 755,
-        y: det.clientY - 390,
+function flagAnimation(){
+    document.addEventListener("mousemove",(det)=>{
+        gsap.to("#flag", {
+            x: det.clientX - 755,
+            y: det.clientY - 390,
+        });
+    })
+    
+    document.querySelector("#hero3").addEventListener("mouseenter", ()=>{
+        gsap.to("#flag", {
+            opacity:1,
+        });
     });
-})
-
-document.querySelector("#hero3").addEventListener("mouseenter", ()=>{
-    gsap.to("#flag", {
-        opacity:1,
+    
+    document.querySelector("#hero3").addEventListener("mouseleave", ()=>{
+        gsap.to("#flag", {
+            opacity:0,
+        });
     });
-});
-
-document.querySelector("#hero3").addEventListener("mouseleave", ()=>{
-    gsap.to("#flag", {
-        opacity:0,
-    });
-});
+    
+}
 
 // Initialize all animations and interactions
 function init() {
     loaderAnimations();
     cursorAnimation();
+    flagAnimation();
     locomotiveScroll();
     sheryanimations();
 }
